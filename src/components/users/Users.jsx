ab7a86c7a2d587aaf69906295ff6046933541106
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2022-08-14 01:15:15 +0300

    добавил axios и get запрос на сервер с получением пользователей

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,29 +1,38 @@
+import axios from "axios";
 import React from "react";
-import classes from "./Users.module.css"
+import classes from "./Users.module.css";
+import user from "../../assets/images/user.png";
+
 let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([{ id: 1, followed: true, fullname: 'Dmitry', status: 'bla bla', location: { city: 'Minsk', country: 'Belarus' } },
-        { id: 2, followed: true, fullname: 'Andrei', status: 'bla bla', location: { city: 'Moscow', country: 'Russia' } },
-        { id: 3, followed: false, fullname: 'Lena', status: 'bla bla', location: { city: 'Rostov-on-don', country: 'Russia' } },
-        { id: 4, followed: false, fullname: 'Sveta', status: 'bla bla', location: { city: 'Krasnodar', country: 'Russia' } }])
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            });
+
+
+        }
     }
     return <div className={classes.wrapper}>
+        <div className={classes.button_wrapper}>
+            <button onClick={getUsers} className={classes.button_getUsers}>Get Users</button>
+        </div>
         {
-            props.users.map(u => <div className={classes.wrapper_user}  key={u.id}>
+            props.users.map(u => <div className={classes.wrapper_user} key={u.id}>
                 <div className={classes.img_wrapper}>
-                    <img src="" alt="" className="" />
+                    <img src={u.photos.small != null ? u.photos.small : user} alt="" className={classes.img} />
                     {u.followed ?
                         <button className={classes.button} onClick={() => { props.unfollow(u.id) }} >unfollow</button> :
                         <button className={classes.button} onClick={() => { props.follow(u.id) }} >follow</button>}
                 </div>
                 <div className={classes.info_wrapper}>
                     <div className={classes.user}>
-                        <p className={classes.user_name}>{u.fullname}</p>
+                        <p className={classes.user_name}>{u.name}</p>
                         <p className={classes.user_status}>{u.status}</p>
                     </div>
                     <div className={classes.location}>
-                        <p className={classes.user_city}>{u.location.city}</p>
-                        <p className={classes.user_country}>{u.location.country}</p>
+                        {/* <p className={classes.user_city}>{u.location.city}</p>
+                        <p className={classes.user_country}>{u.location.country}</p> */}
                     </div>
                 </div>
             </div>)
