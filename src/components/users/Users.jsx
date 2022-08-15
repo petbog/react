4b4fb677c9670ed2de693aec281f6a6725e11c93
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2022-08-16 00:39:52 +0300

    добавил componentDidMount в users

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -4,9 +4,9 @@ import classes from "./Users.module.css";
 import user from "../../assets/images/user.png";
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+
+    componentDidMount(){
+                    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
                 this.props.setUsers(response.data.items)
             });
     }
