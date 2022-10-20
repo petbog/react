6690b6383461bb2b6c284572b8012354912be550
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2022-10-20 15:25:42 +0300

    93 изменение poginator порциями

--- src/components/ItemList/Card_profile/ProfileStatusWithHooks.test.jsx
+++ src/components/ItemList/Card_profile/ProfileStatusWithHooks.test.jsx
@@ -1,44 +1,11 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import classes from './Card_profile.module.css';
+import { create } from "react-test-renderer";
 
-const ProfileStatusWithHooks = (props) => {
-    let [editMode, setEditMode] = useState(false);
-    let [status, setStatus] = useState(props.status);
+describe("Button component", () => {
+  test("Matches the snapshot", () => {
+    const button = create(<Button />);
+    expect(button.toJSON()).toMatchSnapshot();
+  });
+});
 
-    useEffect(()=>{
-        setStatus(props.status)
-    },[props.status])
-
-    let activateEditMode = () => {
-        setEditMode(true);
-    };
-
-    let deActivateEditMode = () => {
-        setEditMode(false);
-        props.updateStatus(status)
-    };
-
-    let onStatusChange=(e)=>{
-        setStatus(e.currentTarget.value);
-    }
-
-    return (
-        <div className={classes.status__box}>
-            {!editMode &&
-                <div className="">
-                    <div className="" onDoubleClick={activateEditMode}>{props.status || "No Status"}</div>
-                </div>
-            }
-            {editMode &&
-                < div className="">
-                    <input type="text" onChange={onStatusChange} onBlur={deActivateEditMode} autoFocus={true} value={status} />
-                </div>
-            }
-        </div>
-    )
-}
-
-
-export default ProfileStatusWithHooks;
+//https://www.valentinog.com/blog/testing-react/ 17:00
--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Paginator.module.css";
 
 
-let Paginator = ({totalUsersCount,pageSize,onPageChanged,currentPage}) => {
+let Paginator = ({ totalUsersCount, pageSize, onPageChanged, currentPage, portionsSize = 10 }) => {
 
     let pagesCount = Math.ceil(totalUsersCount / pageSize);
 
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
-        if (i === 40) break;
+        // if (i === 40) break;
     }
+
+    let portionsCount = Math.ceil(pagesCount / portionsSize);
+    let [portionsNumber, setPortionsNumber] = useState(1);
+    let leftPortionsPageNumber = (portionsNumber - 1) * portionsSize + 1;
+    let rightPortionsPageNumber = portionsNumber * portionsSize;
+
+
     return (
         <div>
-            {pages.map(p => {
+            {portionsNumber > 1 &&
+                <button onClick={() => { setPortionsNumber(portionsNumber - 1) }}> prev</button>}
+            {pages
+            .filter(p => p >= leftPortionsPageNumber && p<= rightPortionsPageNumber)
+            .map(p => {
                 return <span key={p.id} className={currentPage === p && classes.selectedPage}
                     onClick={(e) => { onPageChanged(p); }}> {p}</span>
             })}
+            {
+                portionsCount > portionsNumber &&
+                <button onClick={() => { setPortionsNumber(portionsNumber + 1) }}>next</button>
+            }
         </div>
     )
 }
